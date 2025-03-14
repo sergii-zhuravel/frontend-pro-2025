bfdbcb3b522ec4a90fc777adbf5bd4c7a84e7059
Sergii Zhuravel <szhuravell@gmail.com>
2025-03-14 10:44:11 +0200

    Added exaples

--- arrays_and_functions/arrow-functions.js
+++ arrays_and_functions/arrow-functions.js
@@ -1,3 +1,16 @@
-// () => expression
+// (params)) => expression
 () => 5;
-console.log("test2");
+
+// examples
+() => 2 + 5;
+(oneParam) => console.log(oneParam);
+(a, b) => a + b;
+(a, b) => {
+  return a + b;
+};
+
+//IIFE:  (function)()
+(() => console.log(2 * 5))();
+
+// Return object
+() => ({ id: 1, model: "Audi" });
--- arrays_and_functions/reduce.js
+++ arrays_and_functions/reduce.js
@@ -1,7 +1,21 @@
+// Reduce visualization
+// https://reduce.surge.sh/
+
 const arr = [3, 5, 1, 4, 2];
 const total = arr.reduce((acc, item) => {
   return acc + item;
 }, 0);
 
-// Reduce visualization
-// https://reduce.surge.sh/
+const doubledArray = arr.reduce((acc, item) => {
+  console.log("acc: ", acc);
+  console.log("item: ", item);
+  return [...acc, item * 2];
+}, []);
+
+const arrayToObject = arr.reduce((acc, item) => {
+  console.log("acc: ", acc);
+  console.log("item: ", item);
+  return { ...acc, [item]: item * 2 };
+}, {});
+
+console.log(arrayToObject);
